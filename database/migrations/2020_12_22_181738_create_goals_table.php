4e0e13f7e4160e49be7c9392daa9e349d179172c
xricoxdrumsx <xricoxdrumsx@gmail.com>
2020-12-22 13:26:14 -0500

    Goal DB handlers

---
+++ database/migrations/2020_12_22_181738_create_goals_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGoalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('goals', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 20);
+            $table->string('description', 64)->nullable();
+            $table->bigInteger('goal');
+            $table->bigInteger('saved')->default(0);
+            $table->bigInteger('last')->default(0);
+            $table->date('limit_day')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('goals');
+    }
+}
