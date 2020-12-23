510139504d8a062d50d3a5da3ea20a2cfea41fd3
xricoxdrumsx <xricoxdrumsx@gmail.com>
2020-12-23 15:38:25 -0500

    Added routes for Goals handle

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Goal;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +22,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('goals', 'GoalController');
