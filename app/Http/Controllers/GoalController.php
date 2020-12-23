fa4d63d60ab1bdcf33eac67e407e378e378dadff
xricoxdrumsx <xricoxdrumsx@gmail.com>
2020-12-23 15:39:25 -0500

    Front end testing views and red no data handle

---
+++ app/Http/Controllers/GoalController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Goal;
+use Illuminate\Http\Request;
+
+class GoalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('forms.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return redirect('home');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Goal  $goal
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Goal $goal)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Goal  $goal
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Goal $goal)
+    {
+        return view('forms.edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Goal  $goal
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Goal $goal)
+    {
+        return redirect('home');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Goal  $goal
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Goal $goal)
+    {
+        //
+    }
+}
