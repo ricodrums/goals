2e53eb4164bb9286e1f6a9bf47bf9f897c27a6c3
xricoxdrumsx <xricoxdrumsx@gmail.com>
2020-12-26 15:40:52 -0500

    Visual changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,21 +52,21 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            <li class="nav-item">
+                            <li class="nav-item links">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
                             @if (Route::has('register'))
-                                <li class="nav-item">
+                                <li class="nav-item links">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
-                            <li class="nav-item ">
+                            <li class="nav-item links">
                                 <a class="nav-link" href="#">
                                     {{ Auth::user()->name }}
                                 </a>
                             </li>
-                            <li class="nav-item">
+                            <li class="nav-item links">
                                 <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                                  document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
