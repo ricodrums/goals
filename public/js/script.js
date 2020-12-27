c3e563eb907b95a8877ec605f0cec4e18547f798
xricoxdrumsx <xricoxdrumsx@gmail.com>
2020-12-27 17:38:24 -0500

    Set the data on the modal for payments

---
+++ public/js/script.js
@@ -0,0 +1,6 @@
+function showModal(goal, user) {
+
+    user = document.getElementById('user-id').setAttribute('value', user)
+    goal = document.getElementById('goal-id').setAttribute('value', goal)
+
+}
